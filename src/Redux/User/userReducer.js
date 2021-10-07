0980c89dd17a2f9c150002ff23ad0e158cf46d48
Akiko Green <agreen@alphaworks.tech>
2021-10-07 15:19:57 -0700

    user redux action reducer and type started on

---
+++ src/Redux/User/userReducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USER_REQUEST":
+      return {
+        loading: true,
+      };
+    case "FETCH_USERS_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+      };
+    case "FETCH_USERS_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
